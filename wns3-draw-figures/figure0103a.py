169f47771e5496bfcc92f8e62d84a35f3cb5c1ea
chu <good125628@gmail.com>
2025-01-18 00:48:29 +0800

    upload draw python

---
+++ wns3-draw-figures/figure0103a.py
@@ -0,0 +1,129 @@
+## figure 7a completion time
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = "Times New Roman"
+
+comTime = []
+topDir = '../results-wns3/'
+
+for j in range(1,50):
+    c_time = []
+
+    # one
+    dir = 'one-'+str(j)
+    file = open(topDir+dir+'/scheduler0-queue.txt', 'r')
+    lines = file.readlines()
+    last_line = lines[-1] 
+    thp1 = float(last_line.split('\t')[0])
+    c_time.append(thp1)
+    
+    # two
+    dir = 'two-'+str(j)
+    file = open(topDir+dir+'/scheduler0-queue.txt', 'r')
+    lines = file.readlines()
+    last_line = lines[-1]  
+    thp2 = float(last_line.split('\t')[0])
+    c_time.append(thp2)
+    
+    # four
+    dir = 'four-'+str(j)
+    file = open(topDir+dir+'/scheduler0-queue.txt', 'r')
+    lines = file.readlines()
+    last_line = lines[-1] 
+    thp3 = float(last_line.split('\t')[0])
+    c_time.append(thp3)
+    
+    # 0103
+    dir = 'two-0103-'+str(j)
+    file = open(topDir+dir+'/scheduler0-queue.txt', 'r')
+    lines = file.readlines()
+    last_line = lines[-1] 
+    thp4 = float(last_line.split('\t')[0])
+    c_time.append(thp4)
+    
+    # 0111
+    dir = 'two-0111-'+str(j)
+    file = open(topDir+dir+'/scheduler0-queue.txt', 'r')
+    lines = file.readlines()
+    last_line = lines[-1] 
+    thp5 = float(last_line.split('\t')[0])
+    c_time.append(thp5)
+    
+    # 0103-2
+    dir = 'two-0103-2-'+str(j)
+    file = open(topDir+dir+'/scheduler0-queue.txt', 'r')
+    lines = file.readlines()
+    last_line = lines[-1] 
+    thp6 = float(last_line.split('\t')[0])
+    c_time.append(thp6)
+    
+    comTime.append(c_time)
+
+dataTotal = pd.DataFrame (comTime, columns = ['one', 'two', 'four', '0103', '0111', '0103-2'])
+
+print(dataTotal)
+
+ct0 = [dataTotal['one']]
+ct1 = [dataTotal['two']]
+ct2 = [dataTotal['four']]
+ct3 = [dataTotal['0103']]
+ct4 = [dataTotal['0111']]
+ct5 = [dataTotal['0103-2']]
+ticks = ['one', 'two', 'four', '0103', '0111', '0103-2']
+
+bar_width = 0.9
+
+boxprops = dict(linestyle='-', linewidth=5)
+whiskerprops = dict(linestyle='-', linewidth=5)
+capprops = dict(linestyle='-', linewidth=5)
+medianprops = dict(linestyle='-', linewidth=5)
+
+plt.figure(figsize=(10,8))
+ct_plot0 = plt.boxplot(ct0,positions=np.array(np.arange(len(ct0))),widths=bar_width, patch_artist=True, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops = medianprops)
+for box in ct_plot0['boxes']:
+    box.set(hatch = '/', fill=False) 
+ct_plot1 = plt.boxplot(ct1,positions=np.array(np.arange(len(ct1)))+bar_width+0.1,widths=bar_width, patch_artist=True, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops = medianprops)
+for box in ct_plot1['boxes']:
+    box.set(hatch = 'x', fill=False) 
+ct_plot2 = plt.boxplot(ct2,positions=np.array(np.arange(len(ct2)))+bar_width*2+0.1*2,widths=bar_width, patch_artist=True, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops = medianprops)
+for box in ct_plot2['boxes']:
+    box.set(hatch = '\\', fill=False) 
+ct_plot3 = plt.boxplot(ct3,positions=np.array(np.arange(len(ct3)))+bar_width*3+0.1*3,widths=bar_width, patch_artist=True, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops = medianprops)
+for box in ct_plot3['boxes']:
+    box.set(hatch = '\\', fill=False)
+ct_plot4 = plt.boxplot(ct4,positions=np.array(np.arange(len(ct4)))+bar_width*4+0.1*4,widths=bar_width, patch_artist=True, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops = medianprops)
+for box in ct_plot4['boxes']:
+    box.set(hatch = '\\', fill=False) 
+ct_plot5 = plt.boxplot(ct5,positions=np.array(np.arange(len(ct5)))+bar_width*5+0.1*5,widths=bar_width, patch_artist=True, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops = medianprops)
+for box in ct_plot5['boxes']:
+    box.set(hatch = '\\', fill=False) 
+ 
+
+
+def define_box_properties(plot_name, color_code, label):
+    for k, v in plot_name.items():
+        plt.setp(plot_name.get(k), color=color_code)
+         
+    plt.plot([], c=color_code, label=label)
+ 
+ 
+# setting colors for each groups
+define_box_properties(ct_plot0, 'green', 'one')
+define_box_properties(ct_plot1, 'red', 'two')
+define_box_properties(ct_plot2, 'blue', 'four')
+define_box_properties(ct_plot3, 'yellow', '0103')
+define_box_properties(ct_plot4, 'black', '0111')
+define_box_properties(ct_plot5, '#44dd22', '0103-2')
+
+ticks = ['one', 'two', 'four', '0103', '0111', '0103-2']
+plt.xticks([0,1,2, 3,4,5], ticks)
+plt.xticks(fontsize=24, fontweight='bold')
+plt.yticks(fontsize=24, fontweight='bold')
+plt.ylabel("Completion Time (Seconds)", fontsize=28, fontweight='bold')
+plt.xlabel("Path(s)", fontsize=28, fontweight='bold')
+plt.xlim(-1, len(ticks))
+plt.savefig('../results-wns3/scalable_comtime_0103.png', format='png')
+plt.close()
---
+++ wns3-draw-figures/figure0103b.py
@@ -0,0 +1,188 @@
+## figure 7b instantaneous throughput
+
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = "Times New Roman"
+
+schedulerTypes = [0]
+
+topDir = '../results-wns3/'
+
+def helpone(num,i):
+    dir = topDir+'one-'+str(num)
+    file = open(dir+'/scheduler'+str(i)+'-queue.txt', 'r')
+    finish_time = float(file.readlines()[-1].split('\t')[0])
+    file = open(dir+'/scheduler'+str(i)+'-rx.txt', 'r')
+    lines = file.readlines()
+    goodput = []
+    c_time = []
+    for line in lines:
+        temp = line.split('\t')
+        if (temp[0] == '1' and c_time == []):
+            c_time.append(float(temp[1]))
+            c_time.append(float(temp[5]))
+            c_time.append(i)
+            goodput.append(c_time)
+            if (c_time[0] > finish_time):
+                break
+            c_time = []
+    dataTotal = pd.DataFrame (goodput, columns = ['Time', 'goodput', 'scheduler'])        
+    return dataTotal
+
+def helptwo(num,i):
+    dir = topDir+'two-'+str(num)
+    file = open(dir+'/scheduler'+str(i)+'-queue.txt', 'r')
+    finish_time = float(file.readlines()[-1].split('\t')[0])
+    file = open(dir+'/scheduler'+str(i)+'-rx.txt', 'r')
+    lines = file.readlines()
+    goodput = []
+    c_time = []
+    for line in lines:
+        temp = line.split('\t')
+        if (temp[0] == '1' and c_time == []):
+            c_time.append(float(temp[1]))
+            c_time.append(float(temp[5]))
+        if (temp[0] == '3' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+            c_time.append(i)
+            goodput.append(c_time)
+            if (c_time[0] > finish_time):
+                break
+            c_time = []
+    dataTotal = pd.DataFrame (goodput, columns = ['Time', 'goodput', 'scheduler'])        
+    return dataTotal
+
+def help0103(num,i):
+    dir = topDir+'two-0103-'+str(num)
+    file = open(dir+'/scheduler'+str(i)+'-queue.txt', 'r')
+    finish_time = float(file.readlines()[-1].split('\t')[0])
+    file = open(dir+'/scheduler'+str(i)+'-rx-0103.txt', 'r')
+    lines = file.readlines()
+    goodput = []
+    c_time = []
+    for line in lines:
+        temp = line.split('\t')
+        if (temp[0] == '1' and c_time == []):
+            c_time.append(float(temp[1]))
+            c_time.append(float(temp[5]))
+        if (temp[0] == '3' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+            c_time.append(i)
+            goodput.append(c_time)
+            if (c_time[0] > finish_time):
+                break
+            c_time = []
+    dataTotal = pd.DataFrame (goodput, columns = ['Time', 'goodput', 'scheduler'])        
+    return dataTotal
+
+def help0103_2(num,i):
+    dir = topDir+'two-0103-2-'+str(num)
+    file = open(dir+'/scheduler'+str(i)+'-queue.txt', 'r')
+    finish_time = float(file.readlines()[-1].split('\t')[0])
+    file = open(dir+'/scheduler'+str(i)+'-rx-0103-2.txt', 'r')
+    lines = file.readlines()
+    goodput = []
+    c_time = []
+    for line in lines:
+        temp = line.split('\t')
+        if (temp[0] == '1' and c_time == []):
+            c_time.append(float(temp[1]))
+            c_time.append(float(temp[5]))
+        if (temp[0] == '3' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+            c_time.append(i)
+            goodput.append(c_time)
+            if (c_time[0] > finish_time):
+                break
+            c_time = []
+    dataTotal = pd.DataFrame (goodput, columns = ['Time', 'goodput', 'scheduler'])        
+    return dataTotal
+
+def help0111(num,i):
+    dir = topDir+'two-0111-'+str(num)
+    file = open(dir+'/scheduler'+str(i)+'-queue.txt', 'r')
+    finish_time = float(file.readlines()[-1].split('\t')[0])
+    file = open(dir+'/scheduler'+str(i)+'-rx-0111.txt', 'r')
+    lines = file.readlines()
+    goodput = []
+    c_time = []
+    for line in lines:
+        temp = line.split('\t')
+        if (temp[0] == '1' and c_time == []):
+            c_time.append(float(temp[1]))
+            c_time.append(float(temp[5]))
+        if (temp[0] == '3' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+            c_time.append(i)
+            goodput.append(c_time)
+            if (c_time[0] > finish_time):
+                break
+            c_time = []
+    dataTotal = pd.DataFrame (goodput, columns = ['Time', 'goodput', 'scheduler'])        
+    return dataTotal
+
+
+def helpfour(num,i):
+    dir = topDir+'four-'+str(num)
+    file = open(dir+'/scheduler'+str(i)+'-queue.txt', 'r')
+    finish_time = float(file.readlines()[-1].split('\t')[0])
+    file = open(dir+'/scheduler'+str(i)+'-rx.txt', 'r')
+    lines = file.readlines()
+    goodput = []
+    c_time = []
+    for line in lines:
+        temp = line.split('\t')
+        if (temp[0] == '1' and c_time == []):
+            c_time.append(float(temp[1]))
+            c_time.append(float(temp[5]))
+        if (temp[0] == '3' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+        if (temp[0] == '4' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+        if (temp[0] == '5' and len(c_time) == 2):
+            c_time[1] = c_time[1] + float(temp[5])
+            c_time.append(i)
+            goodput.append(c_time)
+            if (c_time[0] > finish_time):
+                break
+            c_time = []
+    dataTotal = pd.DataFrame (goodput, columns = ['Time', 'goodput', 'scheduler'])        
+    return dataTotal
+
+
+
+## take seed=39 as the example figure
+j = 42
+one = helpone(j,0)
+two = helptwo(j,0)
+two_0103 = help0103(j,0)
+two_0103_2 = help0103_2(j,0)
+two_0111 = help0111(j,0)
+four = helpfour(j,0)  
+print(two_0111)
+
+plt.figure(figsize=(10, 8))
+plt.grid(linestyle="--")  
+ax = plt.gca()
+
+plt.plot(one['Time'], one['goodput'], color="green", label="one path", linewidth=4)
+plt.plot(two['Time'], two['goodput'], color="red", label="two paths", linewidth=4)
+plt.plot(two_0103['Time'], two_0103['goodput'], color="yellow", label="two_0103 paths", linewidth=4)
+plt.plot(two_0103_2['Time'], two_0103_2['goodput'], color="#44dd22", label="two_0103_2 paths", linewidth=4)
+plt.plot(two_0111['Time'], two_0111['goodput'], color="black", label="two_0111 paths", linewidth=4)
+plt.plot(four['Time'], four['goodput'], color="blue", label="four paths", linewidth=4)
+
+plt.xticks(fontsize=24, fontweight='bold')  
+plt.yticks(fontsize=24, fontweight='bold')
+
+plt.ylabel("Instantaneous Throughput (Mbps)", fontsize=26, fontweight='bold')
+plt.xlabel("Time (s)", fontsize=26, fontweight='bold')
+
+plt.legend(loc=4, numpoints=1)
+leg = plt.gca().get_legend()
+ltext = leg.get_texts()
+plt.setp(ltext, fontsize=26, fontweight='bold')
+
+plt.savefig('../results-wns3/ins_throughput_scalable_0103.png', format='png')
+plt.close()
